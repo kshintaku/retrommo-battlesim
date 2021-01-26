9a0860a95453eec0a070b26e91348de50b60232b
Keith <k.shintaku@gmail.com>
2021-01-25 18:01:32 -0800

    missed files

---
+++ src/components/monsters/Monsters.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Spinner from '../layout/Spinner';
+
+const Monsters = ({ loading }) => {
+    if(loading) {
+        return <Spinner />
+    } else {
+        return (
+            
+        )
+    }
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Monsters
